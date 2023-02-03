7dbb8d09514750c9ed07b6abb76b86d6e4ee7383
Sami <78088687+FujiwaraChoki@users.noreply.github.com>
2023-02-03 21:45:59 +0100

    Reaplced all @s with ../

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,55 +1,53 @@
-import Layout from '@/layouts/layout'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
-import BLOG from '@/blog.config'
-import { useRouter } from 'next/router'
-import Loading from '@/components/Loading'
-import NotFound from '@/components/NotFound'
+import Layout from "../layouts/layout";
+import { getAllPosts, getPostBlocks } from "../lib/notion";
+import BLOG from "../blog.config";
+import { useRouter } from "next/router";
+import Loading from "../components/Loading";
+import NotFound from "../components/NotFound";
 
 const Post = ({ post, blockMap }) => {
-  const router = useRouter()
+  const router = useRouter();
   if (router.isFallback) {
-    return (
-      <Loading />
-    )
+    return <Loading />;
   }
   if (!post) {
-    return <NotFound statusCode={404} />
+    return <NotFound statusCode={404} />;
   }
   return (
     <Layout blockMap={blockMap} frontMatter={post} fullWidth={post.fullWidth} />
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  const posts = await getAllPosts({ onlyNewsletter: false })
+  const posts = await getAllPosts({ onlyNewsletter: false });
   return {
     paths: posts.map((row) => `${BLOG.path}/${row.slug}`),
-    fallback: true
-  }
+    fallback: true,
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const posts = await getAllPosts({ onlyNewsletter: false })
-  const post = posts.find((t) => t.slug === slug)
+  const posts = await getAllPosts({ onlyNewsletter: false });
+  const post = posts.find((t) => t.slug === slug);
 
   try {
-    const blockMap = await getPostBlocks(post.id)
+    const blockMap = await getPostBlocks(post.id);
     return {
       props: {
         post,
-        blockMap
+        blockMap,
       },
-      revalidate: 1
-    }
+      revalidate: 1,
+    };
   } catch (err) {
-    console.error(err)
+    console.error(err);
     return {
       props: {
         post: null,
-        blockMap: null
-      }
-    }
+        blockMap: null,
+      },
+    };
   }
 }
 
-export default Post
+export default Post;
