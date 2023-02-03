3692f516fcdc35c7f37e46b18d801213a18eb60f
Sami <78088687+FujiwaraChoki@users.noreply.github.com>
2023-02-03 21:50:35 +0100

    d

--- components/Post/PostFooter.js
+++ components/Post/PostFooter.js
@@ -1,6 +1,6 @@
 import BLOG from "../../blog.config";
 import { useState } from "react";
-import { lang } from "....//lib/lang";
+import { lang } from "../../lib/lang";
 import { useRouter } from "next/router";
 
 import WechatPay from "../../components/Post/WechatPay";
