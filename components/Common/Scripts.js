e62129bba5821f8e35d5167214c804f80aa1f7fc
Sami Hindi <lolipop66611@gmail.com>
2023-02-15 13:57:34 +0100

    removed Strategy from adsense

--- components/Common/Scripts.js
+++ components/Common/Scripts.js
@@ -61,7 +61,6 @@ const Scripts = () => (
       async="true"
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9323484475971782"
       crossOrigin="anonymous"
-      strategy="beforeInteractive"
     />
   </>
 )
