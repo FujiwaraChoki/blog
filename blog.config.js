314b745d9070bd4526a2595af64ade9623cf0085
Sami <78088687+FujiwaraChoki@users.noreply.github.com>
2023-03-04 15:14:32 +0100

    Now not showing newsletter in Navbar anymore

--- blog.config.js
+++ blog.config.js
@@ -1,83 +1,83 @@
 const BLOG = {
-  title: 'Sami Hindi | Blog',
-  author: 'Sami Hindi',
-  email: 'sami@samihindi.com',
-  link: 'https://blog.samihindi.com',
-  newsletter: 'Sami Hindis Blog',
-  description: 'The Blog of Sami Hindi, built with Notion and Next.js',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#212936', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Notionic in a folder
+  title: "Sami Hindi | Blog",
+  author: "Sami Hindi",
+  email: "sami@samihindi.com",
+  link: "https://blog.samihindi.com",
+  newsletter: "Sami Hindis Blog",
+  description: "The Blog of Sami Hindi, built with Notion and Next.js",
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  appearance: "auto", // ['light', 'dark', 'auto'],
+  font: "sans-serif", // ['sans-serif', 'serif']
+  lightBackground: "#F6F8FA", // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: "#212936", // use hex value, don't forget '#'
+  path: "", // leave this empty unless you want to deploy Notionic in a folder
   since: 2022, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
   pagesShow: {
-    newsletter: true,
+    newsletter: false,
     notes: false,
     projects: true,
     contact: true,
     books: false,
     friends: false,
     about: true,
-    videos: true
+    videos: true,
   },
   showWeChatPay: true,
   previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost:
-    'https://og-zl.vercel.app/api/default?border=none&description=Tech%20%26%20Sec%20%26%20News&siteName=Sami%20Hindis%20Blog&summary=Full-Stack%20Software%20Engineer&theme=light&title=Sami%20Hindi', // The link to generate OG image, don't end with a slash
-  defaultCover: 'https://source.unsplash.com/random/800x600?', // The default cover image of your blog, don't end with a slash
+    "https://og-zl.vercel.app/api/default?border=none&description=Tech%20%26%20Sec%20%26%20News&siteName=Sami%20Hindis%20Blog&summary=Full-Stack%20Software%20Engineer&theme=light&title=Sami%20Hindi", // The link to generate OG image, don't end with a slash
+  defaultCover: "https://source.unsplash.com/random/800x600?", // The default cover image of your blog, don't end with a slash
   socialLink: {
-    twitter: 'https://twitter.com/chokifujiwara',
-    github: 'https://github.com/FujiwaraChoki',
-    telegram: 'https://t.me/bv_crack'
+    twitter: "https://twitter.com/chokifujiwara",
+    github: "https://github.com/FujiwaraChoki",
+    telegram: "https://t.me/bv_crack",
   },
   seo: {
-    keywords: ['Sami Hindi', 'GitHub', 'Blog'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    keywords: ["Sami Hindi", "GitHub", "Blog"],
+    googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionSpacesId: process.env.NOTION_SPACES_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  notionDomain: 'sami-hindi.notion.site',
+  notionDomain: "sami-hindi.notion.site",
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
-  telegramChatId: '263895784', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://t.me/+sJuxlgstaE82YjRk', // The link of your Telegram channel
-  telegramChannelName: 'Blog', // The name of your Telegram channel
+  telegramChatId: "263895784", // The chat id of your Telegram bot
+  telegramChannelUrl: "https://t.me/+sJuxlgstaE82YjRk", // The link of your Telegram channel
+  telegramChannelName: "Blog", // The name of your Telegram channel
   analytics: {
-    provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
+    provider: "", // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
     ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.example.com/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.example.com , don't end with a slash
-      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: "", // e.g 'https://ackee.example.com/tracker.js'
+      dataAckeeServer: "", // e.g https://ackee.example.com , don't end with a slash
+      domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     cfConfig: {
-      scriptUrl: 'https://static.cloudflareinsights.com/beacon.min.js', // Default
-      token: '' // Like '{"token": "xxxxxxxxxxxxxxxxxx"}'
+      scriptUrl: "https://static.cloudflareinsights.com/beacon.min.js", // Default
+      token: "", // Like '{"token": "xxxxxxxxxxxxxxxxxx"}'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: "", // e.g: G-XXXXXXXXXX
     },
     umamiConfig: {
-      scriptUrl: '', // The url of your Umami script
-      websiteId: '' // The website id of your Umami instance
-    }
+      scriptUrl: "", // The url of your Umami script
+      websiteId: "", // The website id of your Umami instance
+    },
   },
   comment: {
     // support provider: utterances, supacomments
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: "", // leave it empty if you don't need any comment plugin
     supaCommentsConfig: {
-      supabaseUrl: '', // The url of your Supabase instance
-      supabaseAnonKey: '' // The anonymous key of your Supabase instance
+      supabaseUrl: "", // The url of your Supabase instance
+      supabaseAnonKey: "", // The anonymous key of your Supabase instance
     },
     utterancesConfig: {
-      repo: ''
-    }
+      repo: "",
+    },
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-}
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+};
 // export default BLOG
-module.exports = BLOG
+module.exports = BLOG;
